7820eefade5510731e6138475cde9a325657bc5a
ayumi <dottoz2531@gmail.com>
2024-08-27 23:29:20 +0900

    sp 済

--- js/script.js
+++ js/script.js
@@ -20,6 +20,21 @@ document
     });
   });
 
+const intersectionObserver = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-in-view");
+    } else {
+      entry.target.classList.remove("is-in-view");
+    }
+  });
+});
+
+const inViewItems = document.querySelectorAll(".js-in-view");
+inViewItems.forEach(function (inViewItem) {
+  intersectionObserver.observe(inViewItem);
+});
+
 const swiper = new Swiper("#js-concept-swiper", {
   loopAdditionalSlides: 2,
   spaceBetween: 4,
